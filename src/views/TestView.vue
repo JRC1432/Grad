3fc5e496277cc6ccd36549c30c494849360637bb
Jonathan Castañares <castanares.jon@gmail.com>
2023-07-17 15:46:41 +0800

    d

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,24 +1,42 @@
 <template>
-  <Pie :data="data" :options="options" />
+  <Line :data="linedata" :options="lineoptions" />
 </template>
 
 <script setup>
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const data = {
-  labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
+const linedata = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
   datasets: [
     {
-      backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-      data: [40, 20, 80, 10],
+      label: "Data One",
+      backgroundColor: "#f87979",
+      data: [40, 39, 10, 40, 39, 80, 40],
     },
   ],
 };
 
-const options = {
+const lineoptions = {
   responsive: true,
   maintainAspectRatio: false,
 };
